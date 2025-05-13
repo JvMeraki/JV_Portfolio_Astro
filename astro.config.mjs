a21624eaed02e494e82d86d389378c6e83bcab59
Javi <brayan27_4@live.com>
2025-05-13 14:29:48 -0500

    adding vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'JvMeraki.github.io',
+  site: 'https://JvMeraki.github.io/JV_Portfolio_Astro/',
   base: '/JV_Portfolio_Astro/',
+
   integrations: [
     svelte(),
     tailwind()
   ],
+
+  adapter: vercel(),
 });
