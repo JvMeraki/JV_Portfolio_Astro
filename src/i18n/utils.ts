a66e712eec3ae6c23a79fc82d0b584dfe41381fd
Javi <brayan27_4@live.com>
2026-05-25 01:43:10 -0500

    implement args in i18n

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -2,14 +2,25 @@ import { DEFAULTLANG, LABELS } from './ui';
 
 export function getLangFromUrl(url: URL) {
     const [, lang] = url.pathname.split('/');
-    if (lang in LABELS) {
+    if (lang in LABELS) { 
         return lang as keyof typeof LABELS
     };
     return DEFAULTLANG;
 }
 
 export function useTranslations(lang: keyof typeof LABELS) {
-    return function t(key: keyof typeof LABELS[typeof DEFAULTLANG]) {
-        return LABELS[lang][key] || LABELS[DEFAULTLANG][key];
+    return function t(
+        key: keyof typeof LABELS[typeof DEFAULTLANG],
+        params?: Record<string, string | number>
+    ) {
+        let translation: string = LABELS[lang][key] || LABELS[DEFAULTLANG][key];
+        if (!translation) return "";
+
+        if (params) {
+            Object.entries(params).forEach(([paramKey, paramValue]) => {
+                translation = translation.replace(new RegExp(`{${paramKey}}`, 'g'), String(paramValue));
+            });
+        }
+        return translation;
     }
 }
